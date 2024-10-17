1becb96ad41bce7ee1b5f22a57b4c86307fe9897
JohnGrilo <johngrilo@gmail.com>
2024-10-17 20:47:30 -0300

    Ajustes Tela Sobre

--- src/cep/Sobre.java
+++ src/cep/Sobre.java
@@ -3,6 +3,11 @@ package cep;
 import java.awt.EventQueue;
 
 import javax.swing.JDialog;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.Cursor;
 
 public class Sobre extends JDialog {
 
@@ -27,8 +32,34 @@ public class Sobre extends JDialog {
 	 * Create the dialog.
 	 */
 	public Sobre() {
+		setResizable(false);
+		setTitle("Sobre");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Sobre.class.getResource("/img/home.png")));
 		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Buscar Cep - Ver 1.0");
+		lblNewLabel.setBounds(38, 52, 335, 14);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("@Autor John Grilo - Grilo Company");
+		lblNewLabel_1.setBounds(38, 77, 341, 14);
+		getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("WEB Service:");
+		lblNewLabel_2.setBounds(38, 111, 89, 14);
+		getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("republicavirtual.com.br");
+		lblNewLabel_3.setBounds(126, 111, 132, 14);
+		getContentPane().add(lblNewLabel_3);
+		
+		JButton btnNewButton_1 = new JButton("");
+		btnNewButton_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton_1.setToolTipText("Projeto");
+		btnNewButton_1.setIcon(new ImageIcon(Sobre.class.getResource("/img/github.png")));
+		btnNewButton_1.setBounds(38, 168, 48, 48);
+		getContentPane().add(btnNewButton_1);
 
 	}
-
 }
